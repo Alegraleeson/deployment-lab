af17c875353ca6587455d48b497394085f8e648f
Alegraleeson <wingmonkee@yahoo.com>
2022-07-27 14:27:46 -0500

    updated index.js to make styling work

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
- 
+
 const app = express()
- 
+
+
+// The index.html file has been moved into my public folder so /public needs to be added to the path string. 
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, '../index.html'))
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 })
- 
-// random port from heroku or local server port:
+
+// This is how we can connect the JS and CSS files so they can be accessed by Heroku.
+app.use(express.static(path.join(__dirname, '../public')))
+
 const port = process.env.PORT || 4005
- 
+
 app.listen(port, () => {console.log(`Listening on port ${port}`)})
